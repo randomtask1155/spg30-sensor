d135c4e35cfce693f8eb9e597bf4fa494c22cf98
randomtask1155 <danplynch@gmail.com>
2019-05-09 12:05:47 -0500

    vacuum the database after running clean to avoid table bloat

--- clean-db.sh
+++ clean-db.sh
@@ -2,3 +2,4 @@
 
 
 /usr/bin/psql -U postgres -h 127.0.0.1 gas -c "delete from readings where time < now() - interval '24 hour'"
+/usr/bin/psql -U postgres -h 127.0.0.1 gas -c "vacuum full readings"
